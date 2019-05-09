387ba5c412a8b93d920f7782afea790a75876673
filip.bobek <filip.bobek@aliter.com>
2019-05-09 15:43:44 +0200

    adding bootstrap for data. closes #14

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Owner;
-import com.example.petclinic.services.CrudService;
+import com.example.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,6 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
 
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
---
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,59 @@
+package com.example.petclinic.bootstrap;
+
+import com.example.petclinic.model.Owner;
+import com.example.petclinic.model.Vet;
+import com.example.petclinic.services.OwnerService;
+import com.example.petclinic.services.VetService;
+import com.example.petclinic.services.map.OwnerServiceMap;
+import com.example.petclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService=new OwnerServiceMap();
+        vetService=new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstname("Duro");
+        owner1.setLastname("Killer");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstname("Magda");
+        owner2.setLastname("Sexmachine");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded owners..........");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstname("Kunda");
+        vet1.setLastname("Rozjebana");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstname("Kokot");
+        vet2.setLastname("Povednuty");
+
+        vetService.save(vet2);
+
+        System.out.println("Icte vsetci do PICE!!! :P");
+
+
+    }
+}
