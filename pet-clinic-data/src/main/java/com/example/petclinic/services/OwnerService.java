0545da6317915274f487d61e1b0ced7afa802a93
filip.bobek <filip.bobek@aliter.com>
2019-05-09 09:57:59 +0200

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/example/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/OwnerService.java
@@ -2,10 +2,6 @@ package com.example.petclinic.services;
 
 import com.example.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+public interface OwnerService extends CrudService<Owner,Long>{
+    Owner findByLastName(String lastName);
 }
--- pet-clinic-data/src/main/java/com/example/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/VetService.java
@@ -2,10 +2,6 @@ package com.example.petclinic.services;
 
 import com.example.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
