1f73c73ad89e99fed6bf2a34fa41f0a31186e9f7
filip.bobek <filip.bobek@aliter.com>
2019-05-14 10:17:57 +0200

    added specialities, added to vet. closes #27

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/","/index","/index.html"})
+    @RequestMapping({"","/find","/owners/find","/index","/index.html"})
     public String listOwners(Model model){
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
