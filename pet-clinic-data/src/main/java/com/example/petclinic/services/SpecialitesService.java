b0ca7dccd4cf97e4552e9a48ee90f9fc6ce15075
filip.bobek <filip.bobek@aliter.com>
2019-05-14 13:56:42 +0200

    Added Specialties Map impl, closes #31

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Speciality;
+
+public interface SpecialitesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,33 @@
+package com.example.petclinic.services.map;
+
+import com.example.petclinic.model.Speciality;
+import com.example.petclinic.services.SpecialitesService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
