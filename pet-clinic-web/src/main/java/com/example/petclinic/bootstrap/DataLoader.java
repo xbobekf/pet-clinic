321839385e66f2e6ac8bfbdbbc89a192743b6f2d
filip.bobek <filip.bobek@aliter.com>
2019-05-14 13:44:50 +0200

    added Pets and Contact info to owners on startup. closes #33

--- pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.example.petclinic.bootstrap;
 
 import com.example.petclinic.model.Owner;
+import com.example.petclinic.model.Pet;
 import com.example.petclinic.model.PetType;
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.example.petclinic.services.PetTypeService;
 import com.example.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,14 +40,37 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstname("Duro");
         owner1.setLastname("Killer");
+        owner1.setAddress("123 Street");
+        owner1.setCity("Hell");
+        owner1.setTelephone("1231231233");
 
         ownerService.save(owner1);
+
+        Pet durosPet = new Pet();
+        durosPet.setPetType(savedDogPetType);
+        durosPet.setOwner(owner1);
+        durosPet.setBirthDate(LocalDate.now());
+        durosPet.setName("Galaktus");
+
+        owner1.getPets().add(durosPet);
+
 
         Owner owner2 = new Owner();
         owner2.setFirstname("Magda");
         owner2.setLastname("Sexmachine");
+        owner2.setAddress("123 Street");
+        owner2.setCity("Hell");
+        owner2.setTelephone("1231231233");
 
         ownerService.save(owner2);
+
+        Pet magdasPet = new Pet();
+        magdasPet.setPetType(savedCatPetType);
+        magdasPet.setOwner(owner1);
+        magdasPet.setBirthDate(LocalDate.now());
+        magdasPet.setName("Smaker");
+
+        owner2.getPets().add(magdasPet);
 
         System.out.println("Loaded owners..........");
 
