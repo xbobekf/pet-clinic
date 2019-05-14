f2c85303fead377a4b01ca2ae800edd86495b136
filip.bobek <filip.bobek@aliter.com>
2019-05-14 15:00:22 +0200

    Fixed broken links, closes #45

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -15,9 +15,14 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/find","/owners/find","/index","/index.html"})
+    @RequestMapping({"","/owners","/index","/index.html"})
     public String listOwners(Model model){
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
     }
 }
